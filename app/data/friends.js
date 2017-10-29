cb908d6e97b7a705d1bc41d8671d0dc5c51ec231
Setu Biswas <setubiswas@setus-mbp.attlocal.net>
2017-10-29 14:21:49 -0500

    uploading files

---
+++ app/data/friends.js
@@ -0,0 +1,84 @@
+ var sam = {
+ 	name: "sam",
+ 	photo: "",
+ 	scores: [
+ 		1,
+ 		2,
+ 		4,
+ 		3,
+ 		5,
+ 		3,
+ 		2,
+ 		2,
+ 		1,
+ 		4,
+ 	]
+ };
+
+  var jason = {
+ 	name: "jason",
+ 	photo: "",
+ 	scores: [
+ 		3,
+ 		5,
+ 		2,
+ 		4,
+ 		3,
+ 		1,
+ 		4,
+ 		3,
+ 		5,
+ 		2,
+ 	]
+ };
+
+  var igor = {
+ 	name: "igor",
+ 	photo: "",
+ 	scores: [
+ 		5,
+ 		5,
+ 		2,
+ 		1,
+ 		1,
+ 		5,
+ 		3,
+ 		3,
+ 		2,
+ 		2,
+ 	]
+ };
+
+ var trent = {
+ 	name: "trent",
+ 	photo: "",
+ 	scores: [
+ 		1,
+ 		1,
+ 		3,
+ 		2,
+ 		4,
+ 		4,
+ 		3,
+ 		1,
+ 		5,
+ 		5,
+ 	]
+ };
+
+ var ugi = {
+ 	name: "ugi",
+ 	photo: "",
+ 	scores: [
+ 		4,
+ 		4,
+ 		2,
+ 		1,
+ 		3,
+ 		2,
+ 		5,
+ 		5,
+ 		3,
+ 		2,
+ 	]
+ };
---
+++ server.js
@@ -0,0 +1,6 @@
+// Dependencies
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+var PORT = process.env.PORT || 3000;
+var path = require('path');
