70045f39f8a673928483c79580e2dc6a1ae569e6
Setu Biswas <setubiswas@setus-mbp.attlocal.net>
2017-10-29 15:43:26 -0500

    changed keys

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,52 +1,52 @@
 var friendsArray = [{
-        'name': 'Sam',
-        'photo': '',
-        'scores': [3, 2, 4, 2, 3, 2, 3, 3, 2, 2]
+        name: 'Sam',
+        photo: '',
+        scores: [3, 2, 4, 2, 3, 2, 3, 3, 2, 2]
     },
     {
-        'name': 'Trent',
-        'photo': '',
-        'scores': [5, 1, 3, 1, 2, 1, 5, 5, 2, 1]
+        name: 'Trent',
+        photo: '',
+        scores: [5, 1, 3, 1, 2, 1, 5, 5, 2, 1]
     },
     {
-        'name': 'Jason',
-        'photo': '',
-        'scores': [5, 5, 2, 5, 3, 1, 4, 3, 4, 3]
+        name: 'Jason',
+        photo: '',
+        scores: [5, 5, 2, 5, 3, 1, 4, 3, 4, 3]
     },
     {
-        'name': 'Ugi',
-        'photo': '',
-        'scores': [2, 2, 1, 4, 5, 1, 4, 5, 5, 5]
+        name: 'Ugi',
+        photo: '',
+        scores: [2, 2, 1, 4, 5, 1, 4, 5, 5, 5]
     },
     {
-        'name': 'Igor',
-        'photo': '',
-        'scores': [2, 4, 5, 3, 4, 4, 5, 5, 3, 4]
+        name: 'Igor',
+        photo: '',
+        scores: [2, 4, 5, 3, 4, 4, 5, 5, 3, 4]
     },
     {
-        'name': 'Alyssa',
-        'photo': '',
-        'scores': [1, 1, 3, 1, 5, 5, 1, 1, 1, 2]
+        name: 'Alyssa',
+        photo: '',
+        scores: [1, 1, 3, 1, 5, 5, 1, 1, 1, 2]
     },
     {
-        'name': 'Katie',
-        'photo': '',
-        'scores': [3, 4, 2, 3, 4, 2, 2, 2, 4, 4]
+        name: 'Katie',
+        photo: '',
+        scores: [3, 4, 2, 3, 4, 2, 2, 2, 4, 4]
     },
     {
-        'name': 'Suraya',
-        'photo': '',
-        'scores': [4, 5, 1, 5, 1, 5, 1, 3, 5, 5]
+        name: 'Suraya',
+        photo: '',
+        scores: [4, 5, 1, 5, 1, 5, 1, 3, 5, 5]
     },
     {
-        'name': 'Lindsay',
-        'photo': '',
-        'scores': [4, 2, 1, 4, 1, 1, 4, 4, 3, 1]
+        name: 'Lindsay',
+        photo: '',
+        scores: [4, 2, 1, 4, 1, 1, 4, 4, 3, 1]
     },
     {
-        'name': 'Josh',
-        'photo': '',
-        'scores': [1, 2, 2, 1, 5, 3, 2, 1, 2, 1]
+        name: 'Josh',
+        photo: '',
+        scores: [1, 2, 2, 1, 5, 3, 2, 1, 2, 1]
     },
 ];
 module.exports = friendsArray;
