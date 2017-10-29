aecd61ccd29ab96be2119847fa6e107c8413b4f5
Setu Biswas <setubiswas@setus-mbp.attlocal.net>
2017-10-29 14:57:14 -0500

    changed friends.js file

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,84 +1,52 @@
- var sam = {
- 	name: "sam",
- 	photo: "",
- 	scores: [
- 		1,
- 		2,
- 		4,
- 		3,
- 		5,
- 		3,
- 		2,
- 		2,
- 		1,
- 		4,
- 	]
- };
-
-  var jason = {
- 	name: "jason",
- 	photo: "",
- 	scores: [
- 		3,
- 		5,
- 		2,
- 		4,
- 		3,
- 		1,
- 		4,
- 		3,
- 		5,
- 		2,
- 	]
- };
-
-  var igor = {
- 	name: "igor",
- 	photo: "",
- 	scores: [
- 		5,
- 		5,
- 		2,
- 		1,
- 		1,
- 		5,
- 		3,
- 		3,
- 		2,
- 		2,
- 	]
- };
-
- var trent = {
- 	name: "trent",
- 	photo: "",
- 	scores: [
- 		1,
- 		1,
- 		3,
- 		2,
- 		4,
- 		4,
- 		3,
- 		1,
- 		5,
- 		5,
- 	]
- };
-
- var ugi = {
- 	name: "ugi",
- 	photo: "",
- 	scores: [
- 		4,
- 		4,
- 		2,
- 		1,
- 		3,
- 		2,
- 		5,
- 		5,
- 		3,
- 		2,
- 	]
- };
+var friends = [{
+        “name”: “Sam”,
+        “photo”: ““,
+        “scores”: [3, 2, 4, 2, 3, 2, 3, 3, 2, 2]
+    },
+    {
+        “name”: “Trent”,
+        “photo”: “”,
+        “scores”: [5, 1, 3, 1, 2, 1, 5, 5, 2, 1]
+    },
+    {
+        “name”: “Jason”,
+        “photo”: “”,
+        “scores”: [5, 5, 2, 5, 3, 1, 4, 3, 4, 3]
+    },
+    {
+        “name”: “Ugi”,
+        “photo”: “”,
+        “scores”: [2, 2, 1, 4, 5, 1, 4, 5, 5, 5]
+    },
+    {
+        “name”: “Igor”,
+        “photo”: “”,
+        “scores”: [2, 4, 5, 3, 4, 4, 5, 5, 3, 4]
+    },
+    {
+        “name”: “Alyssa”,
+        “photo”: “”,
+        “scores”: [1, 1, 3, 1, 5, 5, 1, 1, 1, 2]
+    },
+    {
+        “name”: “Katie”,
+        “photo”: “”,
+        “scores”: [3, 4, 2, 3, 4, 2, 2, 2, 4, 4]
+    },
+    {
+        “name”: “Suraya”,
+        “photo”: “”,
+        “scores”: [4, 5, 1, 5, 1, 5, 1, 3, 5, 5]
+    },
+    {
+        “name”: “Lindsay”,
+        “photo”: “”,
+        “scores”: [4, 2, 1, 4, 1, 1, 4, 4, 3, 1]
+    },
+    {
+        “name”: “Josh”,
+        “photo”: “”,
+        “scores”: [1, 2, 2, 1, 5, 3, 2, 1, 2, 1]
+    },
+];
+module.exports = friends;
